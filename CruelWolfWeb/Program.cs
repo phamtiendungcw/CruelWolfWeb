566c19f531ab34e1c1e1877744989eff10c10f65
phamtiendungcw <56197293+phamtiendungcwit@users.noreply.github.com>
2022-06-03 03:02:53 +0700

    Update line Program

--- CruelWolfWeb/Program.cs
+++ CruelWolfWeb/Program.cs
@@ -5,9 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
