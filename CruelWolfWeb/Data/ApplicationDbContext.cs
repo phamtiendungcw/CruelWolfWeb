423d3cf4b0cd1e33ec621f30d919c02a6fc8df4d
phamtiendungcw <56197293+phamtiendungcwit@users.noreply.github.com>
2022-06-03 01:05:13 +0700

    Configuration string connection project to database

---
+++ CruelWolfWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CruelWolfWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CruelWolfWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
--- CruelWolfWeb/Program.cs
+++ CruelWolfWeb/Program.cs
@@ -1,8 +1,13 @@
+using CruelWolfWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
