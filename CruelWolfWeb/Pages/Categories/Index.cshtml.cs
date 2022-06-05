6ef944e5d03689aa21f1fcf0cb5519908886028b
phamtiendungcw <56197293+phamtiendungcwit@users.noreply.github.com>
2022-06-06 02:50:37 +0700

    Categories GET Action Method

---
+++ CruelWolfWeb/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using CruelWolfWeb.Data;
+using CruelWolfWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CruelWolfWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Category> Categories { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            Categories = _db.Category;
+        }
+    }
+}
--- CruelWolfWeb/Program.cs
+++ CruelWolfWeb/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
