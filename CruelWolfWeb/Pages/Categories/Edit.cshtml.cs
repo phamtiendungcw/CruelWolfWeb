3989580bd8831bf974c460bbbbf0618ab255e66a
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-07-09 08:22:35 +0700

    Edit Category

---
+++ CruelWolfWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,42 @@
+using CruelWolfWeb.Data;
+using CruelWolfWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CruelWolfWeb.Pages.Categories;
+
+[BindProperties]
+public class EditModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    public Category Category { get; set; }
+
+    public EditModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet(int id)
+    {
+        Category = _db.Category.Find(id);
+        //Category = _db.Category.FirstOrDefault(u => u.Id == id);
+        //Category = _db.Category.SingleOrDefault(u => u.Id == id);
+        //Category = _db.Category.Where(u => u.Id == id).FirstOrDefault();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (Category.Name == Category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Category.Name", "The DisplayOrder cannot exactly match the Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Category.Update(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+
+        return Page();
+    }
+}
